b5605a2073efb3a5efb56448ea405dacb4566ebb
025georgialynny <46833251+025georgialynny@users.noreply.github.com>
2019-06-11 16:38:39 -0400

    Add files via upload

--- makessamples.R
+++ makessamples.R
@@ -1,19 +1,34 @@
 
 
-path = "/home/georgia/afib/files/csv/"
+path = "/home/georgia/afib/files/csv/" #############path for files - change to your directory
 library(dplyr)
-files = list.files(path, pattern = "*.csv")
+files = list.files(path, pattern = "*.csv") ############pattern of merge files in afib/oldmerge
 for(i in 1:length(files)){
   readfile = paste(path, files[i], sep="")
   assign(files[i], read.csv(readfile))
+}#################read in
+
+
+
+rmlong = function(file, i = nrow(file)){###########################remove any readings of rr intervals greater than 1.5 seconds
+  rm = NULL
+  for(j in 1:i){
+    if(file[j,1]>=1.5){
+      rm = c(rm, j)
+    }
+  }
+  if(!is.null(rm)){
+    file = file[-rm,]}
+  file
 }
 
-head(merge00735.csv)
-      
+for(i in 3:length(files)){##################removes for all files
+  f = rmlong(get(files[i]))
+  assign(files[i], f)
+}
 
-table(merge00735.csv[,3])
 
-afibmean = function(file, i = nrow(file)){
+afibmean = function(file, i = nrow(file)){####################returns running mean for files passed in corresponding to each row entry
   mean = file[1,1]
   tmean = NULL
   for( j in 1:i){
@@ -23,7 +38,7 @@ afibmean = function(file, i = nrow(file)){
   tmean
 }
 
-secs = function(file, i = nrow(file)){
+secs = function(file, i = nrow(file)){########################returns running seconds for files passed in corresponding to each row entry
   sec = 0
   sex = NULL
   for (j in 1:i){
@@ -33,16 +48,21 @@ secs = function(file, i = nrow(file)){
   sex
 }
 
-for(i in 1:length(files)){
+for(i in 1:length(files)){###############running mean for each file
   me = afibmean(get(files[i]))
   assign(files[i], cbind(get(files[i]), me))
 }
 
-for(i in 1:length(files)){
+for(i in 1:length(files)){################running seconds for each entry
   sec = secs(get(files[i]))
   assign(files[i], cbind(get(files[i]), sec))
 }
+
+
+
 ################## rr, samp, n, rmean, rsec
+
+#################################plotting
 rmeanplot = function(fullfile){
   par(mfrow = c(2, 1), mar = c(5,5, 1,1))
   colnames(fullfile) = c("rr", "samp", "afib", "rmean", "rsec")
@@ -86,27 +106,27 @@ rmeanplot(merge08455.csv)
 
 ################## rr, samp, n, rmean, rsec
 
-classify= function(file, i = nrow(file)){
+classify= function(file, i = nrow(file)){################################add a column categorizing each rr interval as long short or regular
   tran = NULL
   for(j in 1:i){
     if(file[j,1]>(1.15*file[j,4])){
       tran = c(tran, 1) ######################                     1 = L
     }else if(file[j,1]<(.85*file[j,4])){
-      tran = c(tran, 3) ####################                        2 = S
+      tran = c(tran, 3) ####################                        3 = S
     }else{
-      tran = c(tran, 2)  ###################                        3 = R
+      tran = c(tran, 2)  ###################                        2 = R
     }
   }
   tran
 }
 
-for(i in 3:length(files)){
+for(i in 3:length(files)){############classify each rr interval for each file
   trans = classify(get(files[i]))
   assign(files[i], cbind(get(files[i])[,-6], trans))
 }
 
 
-transmat = function(file, mat){
+transmat = function(file, mat){###############creates transition matrix based on classify function additions above
   for(i in 1:(nrow(file)-1)){
     from = file[i,6]
     to = file[i+1, 6]
@@ -118,35 +138,35 @@ transmat = function(file, mat){
 
 trans = NULL
 samps = NULL
-for (i in 1:length(files)){
+for (i in 1:length(files)){##########creates trans and samps lists for easy access to trans and samps data
   trans = c(trans, paste("trans", substr(files[i], 6, 10), sep = ""))
   samps = c(samps, paste("samps", substr(files[i], 6, 10), sep = ""))
 }
 
 
-for(i in 3:length(files)){
+for(i in 3:length(files)){#################makes transition matrices
   mat = matrix(rep(0, 9), ncol = 3)
   assign(trans[i], transmat(get(files[i]), mat))
 }
 
-for(i in 3:length(trans)){
+for(i in 3:length(trans)){#################prints transition matrices
   print(trans[i])
   print(get(trans[i]))
 }
 
-props = function(mat){
+props = function(mat){#####################function to turn transition matrices into proportions instead of tallies of individual beats
   summy = sum(mat)
   mat = mat/summy
   unlist(list(mat))
 }
 
-sampfib = function(file, i = nrow(file)){
+sampfib = function(file, i = nrow(file)){######################function determine if a sample group should be classified as afib or not
   afib = length(which(file[,3]=="B"))
   non = length(which(file[,3]!="B"))
   afib>non
 }
 
-sub = function(file, i = nrow(file), id){
+sub = function(file, i = nrow(file), id){#######################3function to split individual data frames into samples of 45 secons each
   subs = NULL
   sub1 = NULL
   sums = 0
@@ -181,12 +201,12 @@ sub = function(file, i = nrow(file), id){
 }
 
 
-for(j in 3:length(files)){
+for(j in 3:length(files)){###################makes each file into sample
   assign(samps[j], sub(get(files[j]), id = substr(files[j], 6, 10)))
 }
 
 
-for( i in 3:length(files)){
+for( i in 3:length(files)){################################give meaningful col names to each file used (trans, files, and samps)
   s = get(samps[i])
   f = get(files[i])
   t = get(trans[i])
@@ -200,9 +220,9 @@ for( i in 3:length(files)){
   assign(files[i], f)
 }
 
-path = "/home/georgia/afib/samplesout/"
+path = "/home/georgia/afib/samplesout/"#########################33change path for yourself
 
-for(i in 3:length(files)){
+for(i in 3:length(files)){###############################################3write out all new data
   write.csv(get(samps[i]), paste(path, samps[i], ".csv", sep = ""))
   
   write.csv(get(trans[i]), paste(path, trans[i], ".csv", sep = ""))
